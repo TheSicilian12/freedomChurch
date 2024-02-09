01b83e7a4618620c9014a540a7573ed5567a9aa6
TheSicilian12 <mguidera314@gmail.com>
2024-02-09 11:24:30 -0500

    added font awesome link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,24 @@
 import churchLogo from '../images/ChurchLogo.svg'
+import hamburgerMenu from '../images/icons/hamburger-menu.svg'
 
 export function NavBar() {
     return (
         <>
-            <div class="navbar-container">
+            <div class="navbar-container border">
                 <div class="navbar-left">
                     <div class="homepage-navbar-left">
                         <img src={churchLogo} />
                     </div>
                 </div>
 
-                <div>
-                    <div class="navbar-location-container"></div>
-                    <div></div>
+                <div class="navbar-right border">
+                    <div class="navbar-location-container">Location
+                        <div class="navbar-location-options"></div>
+
+                    </div>
+                    {/* <img src={hamburgerMenu} class="navbar-hamburger border"/> */}
+                    <i class="fa fa-bars"></i>
+                    Hello
                 </div>
             </div>
         </>
