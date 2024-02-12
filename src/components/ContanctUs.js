4d42627af86645b9e60e5b9dda88a6c341e4f2f0
TheSicilian12 <mguidera314@gmail.com>
2024-02-12 11:53:02 -0500

    wokring on the general style of this

--- src/components/ContanctUs.js
+++ src/components/ContanctUs.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export function ContanctUs() {
-    const [firstName, setFirstName] = useState("first");
-    const [lastName, setLastName] = useState("last");
-    const [email, setEmail] = useState("email");
-    const [phone, setPhone] = useState("");
-    const [message, setMessage] = useState("");
+    const [firstName, setFirstName] = useState("First name");
+    const [lastName, setLastName] = useState("Last name");
+    const [email, setEmail] = useState("Email");
+    const [phone, setPhone] = useState("Phone");
+    const [message, setMessage] = useState("Question or message");
 
     const sendMessage = () => {
         console.log("send message")
@@ -15,28 +15,33 @@ export function ContanctUs() {
     return (
         <>
             <div className="contact-container input-label">
-                <div>Name</div>
-                <div className="contact-name-fields">
-                    <input
-                        className="contact-input-small contact-input"
-                        type="text"
-                        // value={firstName}
-                        // placeholder={firstName}
-                        onChange={(e) => {
-                            setFirstName(e.target.value)
-                        }}></input>
 
-                    <input
-                        className="contact-input-small contact-input"
-                        // value={lastName}
-                        onChange={(e) => {
-                            setLastName(e.target.value)
-                        }}></input>
+                <div>Name</div>
+                <div>
+                    <div className="contact-name-fields">
+                        <input
+                            className="contact-input-small contact-input"
+                            type="text"
+                            // value={firstName}
+                            placeholder={firstName}
+                            onChange={(e) => {
+                                setFirstName(e.target.value)
+                            }}></input>
+
+                        <input
+                            className="contact-input-small contact-input"
+                            // value={lastName}
+                            placeholder={lastName}
+                            onChange={(e) => {
+                                setLastName(e.target.value)
+                            }}></input>
+                    </div>
                 </div>
 
                 <label>Email</label>
                 <input
                     className="contact-input-small contact-input"
+                    placeholder={email}
                     onChange={(e) => {
                         setEmail(e.target.value)
                     }}></input>
@@ -44,14 +49,15 @@ export function ContanctUs() {
                 <label>Phone Number</label>
                 <input
                     className="contact-input-small contact-input"
+                    placeholder={phone}
                     onChange={(e) => {
                         setPhone(e.target.value)
                     }}></input>
 
                 <label>Question or message</label>
                 <textarea
-                    type="text"
                     className="contact-input-large contact-input"
+                    placeholder={message}
                     onChange={(e) => {
                         setMessage(e.target.value)
                     }}></textarea>
