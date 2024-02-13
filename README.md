108f327d7b113a8a55701f65b1e79aabe53bdf7a
TheSicilian12 <mguidera314@gmail.com>
2024-02-13 16:38:13 -0500

    added an npm package for react slide

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Packages:
+react-slide-toggle
+https://www.npmjs.com/package/react-slide-toggle
+
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/InfoDrop.js
+++ src/components/InfoDrop.js
@@ -1,7 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import SlideToggle from "react-slide-toggle";
 
 export function InfoDrop() {
     const [infoVisible, setInfoVisible] = useState(false);
+    const contentRef = useRef(null);
+    const [contentHeight, setContentHeight] = useState(0);
+
+    useEffect(() => {
+        if (contentRef.current) {
+            setContentHeight(infoVisible ? contentRef.current.scrollHeight : 0);
+        }
+    }, [infoVisible]);
+
+    console.log(contentHeight)
 
     const infoToggle = () => {
         setInfoVisible(!infoVisible);
@@ -9,13 +20,16 @@ export function InfoDrop() {
 
     return (
         <>
-            <div className="border"
-                onClick={infoToggle}>
-                <i className="fa fa-angle-down"></i>
+            <div className="info-header-container border" onClick={infoToggle}>
+                <i className='fa fa-angle-down'></i>
                 <div>Info Header</div>
             </div>
 
-            <div className={`info-content ${infoVisible ? "visible" : "hidden"}`}>Info Content</div>
+            <div className={`info-content-container ${infoVisible ? "visible" : "hidden"}`} style={{ maxHeight: contentHeight }}>
+                <div ref={contentRef} className={`info-content ${infoVisible ? "visible" : "hidden"}`}>
+                    Info Content
+                </div>
+            </div>
         </>
-    )
+    );
 }
