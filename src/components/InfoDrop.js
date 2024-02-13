1f2b6b567ce487117ff0403257489fae897cf441
TheSicilian12 <mguidera314@gmail.com>
2024-02-13 16:44:06 -0500

    toggle working

--- src/components/InfoDrop.js
+++ src/components/InfoDrop.js
@@ -6,30 +6,23 @@ export function InfoDrop() {
     const contentRef = useRef(null);
     const [contentHeight, setContentHeight] = useState(0);
 
-    useEffect(() => {
-        if (contentRef.current) {
-            setContentHeight(infoVisible ? contentRef.current.scrollHeight : 0);
-        }
-    }, [infoVisible]);
-
-    console.log(contentHeight)
-
-    const infoToggle = () => {
-        setInfoVisible(!infoVisible);
-    }
 
     return (
         <>
-            <div className="info-header-container border" onClick={infoToggle}>
-                <i className='fa fa-angle-down'></i>
-                <div>Info Header</div>
-            </div>
-
-            <div className={`info-content-container ${infoVisible ? "visible" : "hidden"}`} style={{ maxHeight: contentHeight }}>
-                <div ref={contentRef} className={`info-content ${infoVisible ? "visible" : "hidden"}`}>
-                    Info Content
-                </div>
-            </div>
+            <SlideToggle
+                collapsed
+                duration={500}>
+                {({ toggle, setCollapsibleElement }) => (
+                    <div className="my-collapsible">
+                        <button className="my-collapsible__toggle" onClick={toggle}>
+                            toggle
+                        </button>
+                        <div className="my-collapsible__content" ref={setCollapsibleElement}>
+                            <div className="my-collapsible__content-inner">Collapsible content</div>
+                        </div>
+                    </div>
+                )}
+            </SlideToggle>
         </>
     );
 }
