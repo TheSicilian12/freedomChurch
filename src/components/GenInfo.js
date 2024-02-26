56b1cecb299dd18717fbf805fe2070ed814dbd0e
TheSicilian12 <mguidera314@gmail.com>
2024-02-26 12:02:06 -0500

    save point

--- src/components/GenInfo.js
+++ src/components/GenInfo.js
@@ -5,6 +5,11 @@ export function GenInfo() {
     return(
         <>
             <div>General Info</div>
+
+            <div class="genInfo-symbol"></div>
+            <div class="genInfo-header"></div>
+            <div class="genInfo-body"></div>
+            <div class="genInfo-img"></div>
         </>
     )
 }
