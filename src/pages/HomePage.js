9fe734560cee0f299a1afc016ab432cddbea1ba0
TheSicilian12 <mguidera314@gmail.com>
2024-02-26 11:55:23 -0500

    added background img for header

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import { NavBar } from '../components/NavBar';
 import { ContanctUs } from '../components/ContanctUs';
 import { InfoDrop } from '../components/InfoDrop';
 
+import homePageImg from '../images/freedomChurchHomepage.jpeg'
+
 export function HomePage() {
     const infoDropHeader = "Header"
     const infoDropContent = "Content"
@@ -9,11 +11,16 @@ export function HomePage() {
     return (
         <>
             <NavBar />
+            {/* <img src={homePageImg}
+                class="homepage-header-img"/> */}
+            <div class="homepage-header"></div>
+
+
             <div className="margin-large">Home page</div>
 
-            {/* <div className="border">
+            <div className="border">
                 <ContanctUs />
-            </div> */}
+            </div>
 
             <InfoDrop header={infoDropHeader}
                 headerSize={"medium-font"}
