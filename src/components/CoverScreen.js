47cf659e19e3b1486f7b47049ba88b1cb0fa4a54
TheSicilian12 <mguidera314@gmail.com>
2024-03-01 11:33:31 -0500

    thinking through the menu screen

--- src/components/CoverScreen.js
+++ src/components/CoverScreen.js
@@ -2,7 +2,7 @@ export function CoverScreen() {
 
     return (
         <>
-            <div className="bg-color-main">
+            <div className="bg-color-main vw-100 vh-100 scroll-none">
                 Hello
             </div>
         </>
