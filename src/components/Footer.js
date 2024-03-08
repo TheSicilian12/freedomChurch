c9ecfec123a1ed857381f44e2edecdc212c82afd
TheSicilian12 <mguidera314@gmail.com>
2024-03-08 13:50:13 -0500

    basic css for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,16 @@
 export function Footer() {
 
     return (
-        <div className="display-flex-col flex-vcenter">
-            <div className="border">
+        <div className="display-flex-col flex-vcenter bg-color-main">
+            <div className="m-10">
                 <div className="xlarge-font">Freedom Church</div>
-                <div className="medium-font">Two locations</div>
-                <div className="medium-font">One body</div>
+                <div className="display-flex-col flex-vcenter">
+                    <div className="medium-font">Two locations</div>
+                    <div className="medium-font">One body</div>
+                </div>
             </div>
 
-            <div className="display-flex">
+            <div className="display-flex m-10">
                 <div>
                     <div className="medium-font">Location 1</div>
                     <div className="small-font">Address</div>
