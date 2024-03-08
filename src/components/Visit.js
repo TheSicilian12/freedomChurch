b7e7a50ccecef952b8a954f1e39a4e045b290de8
TheSicilian12 <mguidera314@gmail.com>
2024-03-08 13:25:46 -0500

    css for visit.js

--- src/components/Visit.js
+++ src/components/Visit.js
@@ -13,42 +13,43 @@ export function Visit() {
             </div>
 
             <div className="display-flex">
-                <div className="display-flex-col  vw-35 experience-genInfo">
-                    <InfoDrop header="WHAT SHOULD I WEAR?"
-                        headerSize={"small-font"}
-                        content="We want church to be a place you feel comfortable so you won't find a dress code here. You'll see most people wearing jeans and t-shirts, but pollos and button ups are common too."
-                        contentSize={"xsmall-font"}
-                        arrowSize={"fa-xs"}
-                    />
+                    <div className="display-flex-col flex-between vw-35 experience-genInfo">
+                        <InfoDrop header="WHAT SHOULD I WEAR?"
+                            headerSize={"small-font"}
+                            content="We want church to be a place you feel comfortable so you won't find a dress code here. You'll see most people wearing jeans and t-shirts, but pollos and button ups are common too."
+                            contentSize={"xsmall-font"}
+                            arrowSize={"fa-xs"}
+                        />
 
-                    <InfoDrop header="WHAT IS A TYPICAL SERVICE LIKE?"
-                        headerSize={"small-font"}
-                        content="Typically around 10:30 AM, our service begins with some worship. After that we have prayer and announcements. We then have a small break where kids 12 and under are released to children's church. Everyone grabs donuts and coffee before coming back for the sermon. You'll see the teens all like to sit together in our front two rows! After the sermon, we close with prayer and then all get more donuts and coffee before heading home."
-                        contentSize={"xsmall-font"}
-                        arrowSize={"fa-xs"}
-                    />
+                        <InfoDrop header="WHAT IS A TYPICAL SERVICE LIKE?"
+                            headerSize={"small-font"}
+                            content="Typically around 10:30 AM, our service begins with some worship. After that we have prayer and announcements. We then have a small break where kids 12 and under are released to children's church. Everyone grabs donuts and coffee before coming back for the sermon. You'll see the teens all like to sit together in our front two rows! After the sermon, we close with prayer and then all get more donuts and coffee before heading home."
+                            contentSize={"xsmall-font"}
+                            arrowSize={"fa-xs"}
+                        />
 
-                    <InfoDrop header="WHAT DO YOU HAVE FOR MY KIDS?"
-                        headerSize={"small-font"}
-                        content="For kids ages 5 - 12, we have a childrens church! For kids younger than that, you'll find a nursery with plenty of toys, a tv, and speakers to listen to the main sermon."
-                        contentSize={"xsmall-font"}
-                        arrowSize={"fa-xs"}
-                    />
+                        <InfoDrop header="WHAT DO YOU HAVE FOR MY KIDS?"
+                            headerSize={"small-font"}
+                            content="For kids ages 5 - 12, we have a childrens church! For kids younger than that, you'll find a nursery with plenty of toys, a tv, and speakers to listen to the main sermon."
+                            contentSize={"xsmall-font"}
+                            arrowSize={"fa-xs"}
+                        />
 
-                    <InfoDrop header="WHERE DO I GO WHEN I ARRIVE?"
-                        headerSize={"small-font"}
-                        content="The elevator leads up to the main lobby where we keep our donuts and coffee."
-                        contentSize={"xsmall-font"}
-                        arrowSize={"fa-xs"}
-                    />
+                        <InfoDrop header="WHERE DO I GO WHEN I ARRIVE?"
+                            headerSize={"small-font"}
+                            content="The elevator leads up to the main lobby where we keep our donuts and coffee."
+                            contentSize={"xsmall-font"}
+                            arrowSize={"fa-xs"}
+                        />
 
-                    <InfoDrop header="DO I NEED TO BE A MEMBER?"
-                        headerSize={"small-font"}
-                        content="No, all are welcome to come and have fun. The only requirement is that you eat as many donuts as you want!"
-                        contentSize={"xsmall-font"}
-                        arrowSize={"fa-xs"}
-                    />
-                </div>
+                        <InfoDrop header="DO I NEED TO BE A MEMBER?"
+                            headerSize={"small-font"}
+                            content="No, all are welcome to come and have fun. The only requirement is that you eat as many donuts as you want!"
+                            contentSize={"xsmall-font"}
+                            arrowSize={"fa-xs"}
+                        />
+                    </div>
+
 
                 <div className="">
                     <ContanctUs />
