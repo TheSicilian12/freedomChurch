c1dd5882079149169f21e1679d4823727fc07feb
TheSicilian12 <mguidera314@gmail.com>
2024-03-09 10:19:39 -0500

    basic footer done:

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,23 +12,30 @@ export function Footer() {
 
             <div className="display-flex flex-even m-10">
 
-                <div className="display-flex-col vw-50 flex-hcenter flex-vcenter border">
-                    <div className="medium-font">Location 2</div>
-                    <div className="small-font">Address</div>
-                    <div className="small-font">Service Start Time</div>
+                <div className="display-flex-col vw-50 flex-hcenter flex-vcenter border-white">
+                    <div className="medium-font">Lima, Ohio</div>
+
+                    <div className="xsmall-font m-10">Service: 10:30 AM</div>
+
+                    <div className="m-10">
+                        <div className="xsmall-font">2244 Baton Rouge</div>
+                        <div className="xsmall-font">Lima OH 45805</div>
+                    </div>
                 </div>
 
-                <div className="display-flex-col vw-50 flex-hcenter flex-vcenter border">
+                <div className="display-flex-col vw-50 flex-hcenter flex-vcenter border-white-tbr">
                     <div className="medium-font">Ottawa, Ohio</div>
-                    <div className="ml-10">
+
+                    <div className="xsmall-font m-10">Service: 10:30 AM</div>
+
+                    <div className="m-10">
                         <div className="xsmall-font">133 N Court St</div>
                         <div className="xsmall-font">3rd Floor, 307C</div>
                         <div className="xsmall-font">Ottawa, OH 45875</div>
                     </div>
-
-                    <div className="xsmall-font">Service: 10:30 AM</div>
                 </div>
             </div>
+
         </div>
     )
 }
