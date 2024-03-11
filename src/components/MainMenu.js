b00dd356fe259316d72b5a7757b74b32f7da5b92
TheSicilian12 <mguidera314@gmail.com>
2024-03-11 11:49:47 -0400

    basic structure

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -5,55 +5,59 @@ export function MainMenu() {
     return (
         <div className="pt-100 font-color-white">
 
-            <div className="main-menu-grid border vw-70 vh-70 ml-10">
+            <div className="main-menu-grid vw-70 vh-70 ml-50">
                 <div className="">
                     <div className="large-font m-10">About</div>
                     <div className="cursor-pointer">
-                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
-                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
-                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
                     </div>
-
                 </div>
 
                 <div>
-                    <div className="medium-font">About</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
                 </div>
 
                 <div>
-                    <div className="medium-font">About</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
                 </div>
 
                 <div>
-                    <div className="medium-font">About</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
                 </div>
 
                 <div>
-                    <div className="medium-font">About</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
                 </div>
 
                 <div>
-                    <div className="medium-font">About</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-                    <div>Mission and Vision</div>
-
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                        <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
                 </div>
             </div>
 
