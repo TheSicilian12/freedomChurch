8242aca4ac6ae9a05eb8933e30677cab67863ca7
TheSicilian12 <mguidera314@gmail.com>
2024-03-11 11:47:42 -0400

    working on basic styling

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -6,6 +6,16 @@ export function MainMenu() {
         <div className="pt-100 font-color-white">
 
             <div className="main-menu-grid border vw-70 vh-70 ml-10">
+                <div className="">
+                    <div className="large-font m-10">About</div>
+                    <div className="cursor-pointer">
+                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    <div className="xsmall-font m-10 underline-hover">Mission and Vision</div>
+                    </div>
+
+                </div>
+
                 <div>
                     <div className="medium-font">About</div>
                     <div>Mission and Vision</div>
@@ -15,23 +25,35 @@ export function MainMenu() {
                 </div>
 
                 <div>
-                    Watch
+                    <div className="medium-font">About</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+
                 </div>
 
                 <div>
-                    Get Involved
+                    <div className="medium-font">About</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+
                 </div>
 
                 <div>
-                    Connect
+                    <div className="medium-font">About</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+
                 </div>
 
                 <div>
-                    Ministries
-                </div>
+                    <div className="medium-font">About</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
+                    <div>Mission and Vision</div>
 
-                <div>
-                    Resources
                 </div>
             </div>
 
