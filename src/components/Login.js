840a450072fd253bdde4542144943ec94352382c
TheSicilian12 <mguidera314@gmail.com>
2024-03-13 11:44:58 -0400

    the issue is directly connected to the use of useState in the login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
+// NOT SECURE! DO NOT ACTUALLY USE UNTIL SECURITY CAN BE ADDED
 export function Login() {
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
 
     return (
         <div className="pt-125 font-color-white display-flex flex-between">
-            Hello
+            <div>Freedom Church</div>
+           {/* <form>
+                <div>
+                    <label>Username</label>
+                    <input
+                        type='text'
+                        value={username}
+                        onChange={(e) => {
+                            setUsername(e.target.value)
+                        }}/>
+                </div>
 
+                <div>
+                    <label>Password</label>
+                    <input
+                        type='text'
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}/>
+                </div>
+           </form> */}
         </div>
     )
 }
