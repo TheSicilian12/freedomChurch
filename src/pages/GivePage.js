7419bb016b35a0bde6dd904207b9c696d0dfb826
TheSicilian12 <mguidera314@gmail.com>
2024-03-15 11:06:41 -0400

    working on basic structure

--- src/pages/GivePage.js
+++ src/pages/GivePage.js
@@ -3,7 +3,22 @@ import React from 'react';
 export function GivePage() {
     return (
         <>
-            <div>Give page</div>
+            <div>
+                <div>3 Simple and Safe Ways to Give</div>
+                <div>
+                    <div>Give on Sunday</div>
+                    <div>Place your offering in our tithe box.</div>
+                </div>
+                <div>
+                    <div>Give Online</div>
+                    <div>Our giving platform is safe and secure.</div>
+                </div>
+                <div>
+                    <div>Give by Mail</div>
+                    <div>Please send your offering to any of our locations.</div>
+                </div>
+            </div>
+            <div></div>
         </>
     )
 }
