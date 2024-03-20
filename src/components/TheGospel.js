c06d2a0325c28775261b01ba622a85d1e98b6b00
TheSicilian12 <mguidera314@gmail.com>
2024-03-20 13:20:52 -0400

    adding gospel infodrop

--- src/components/TheGospel.js
+++ src/components/TheGospel.js
@@ -5,9 +5,44 @@ import { InfoDrop } from '../components/InfoDrop';
 export function TheGospel() {
     return (
         <>
-            <InfoDrop header="WHAT SHOULD I WEAR?"
+            <InfoDrop header="God Is Sovereign Creator"
                 headerSize={"small-font"}
-                content="We want church to be a place you feel comfortable so you won't find a dress code here. You'll see most people wearing jeans and t-shirts, but pollos and button ups are common too."
+                content="Contemporary thinking says man is the product of evolution. But the Bible says we were created by a personal God to love, serve, and enjoy endless fellowship with Him. The New Testament reveals it was Jesus Himself who created everything (John 1:3; Colossians 1:16). Therefore, He also owns and rules everything (Psalm 103:19). That means He has authority over our lives and we owe Him absolute allegiance, obedience, and worship."
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+
+            <InfoDrop header="God Is Holy"
+                headerSize={"small-font"}
+                content="God is absolutely and perfectly holy (Isaiah 6:3); therefore He cannot commit or approve of evil (James 1:13). God requires holiness of us as well. First Peter 1:16 says, You shall be holy, for I am holy."
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+
+            <InfoDrop header="Mankind Is Sinful"
+                headerSize={"small-font"}
+                content="holiness and Scripture, everyone is guilty of sin: There is no man who does not sin (1 Kings 8:46). That doesn't mean we're incapable of performing acts of human kindness. But we're utterly incapable of understanding, loving, or pleasing God on our own (Romans 3:10-12)."
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+
+            <InfoDrop header="Sin Demands a Penalty"
+                headerSize={"small-font"}
+                content="God's holiness and justice demand that all sin be punished by eternal death (Ezekiel 18:4; Romans 6:23). That's why simply changing our patterns of behavior can't solve our sin problem or eliminate its consequences."
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+
+            <InfoDrop header="Sin Demands a Penalty"
+                headerSize={"small-font"}
+                content="Jesus Is Lord and Savior"
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+
+            <InfoDrop header="The Character of Saving Faith"
+                headerSize={"small-font"}
+                content="Romans 10:9 says, True faith is always accompanied by repentance from sin. Repentance is agreeing with God that you are sinful, confessing your sins to Him, and making a conscious choice to turn from sin (Luke 13:3, 5; 1 Thessalonians 1:9), pursue Christ (Matthew 11: 28-30; John 17:3), and obey Him (1 John 2:3). It isn't enough to believe certain facts about Christ. Even Satan and his demons believe in the true God (James 2:19), but they don't love and obey Him. True saving faith always responds in obedience (Ephesians 2:10)."
                 contentSize={"xsmall-font"}
                 arrowSize={"fa-xs"}
             />
