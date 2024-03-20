07f47b23564bd0ad70cfce940deb6d9e690105c2
TheSicilian12 <mguidera314@gmail.com>
2024-03-20 13:18:09 -0400

    adding drop down for gospel page

---
+++ src/components/TheGospel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { InfoDrop } from '../components/InfoDrop';
+
+export function TheGospel() {
+    return (
+        <>
+            <InfoDrop header="WHAT SHOULD I WEAR?"
+                headerSize={"small-font"}
+                content="We want church to be a place you feel comfortable so you won't find a dress code here. You'll see most people wearing jeans and t-shirts, but pollos and button ups are common too."
+                contentSize={"xsmall-font"}
+                arrowSize={"fa-xs"}
+            />
+        </>
+    )
+}
--- src/pages/BeliefPage.js
+++ src/pages/BeliefPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { NavBar } from '../components/NavBar';
-import { Button } from '../components/Button';
+// import { Button } from '../components/Button';
+import { TheGospel } from '../components/TheGospel';
 
 export function BeliefPage() {
     return (
-        <>
+        <div className="pt-100">
             <NavBar />
-        </>
+            <TheGospel />
+        </div>
     )
 }
