d8a52fc2a051dc93faa5f53e44cf20c9094ef240
TheSicilian12 <mguidera314@gmail.com>
2024-03-20 13:12:56 -0400

    update

--- src/pages/BeliefPage.js
+++ src/pages/BeliefPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { NavBar } from '../components/NavBar';
+import { Button } from '../components/Button';
 
 export function BeliefPage() {
     return (
         <>
             <NavBar />
-
-            
         </>
     )
 }
